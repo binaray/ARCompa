e74f74765ec55098e93c0e06893610ecc264ceee
binaray@popon <raychengcx@live.com>
2018-09-29 22:24:47 +0800

    poke-able

--- Assets/PetController.cs
+++ Assets/PetController.cs
@@ -16,7 +16,8 @@ public class PetController : MonoBehaviour {
 	public float alpha = 0.6f;
 	public float beta = 0.4f;
 	private States state;
-	public Action action;
+    private Transform actionTarget = null;
+    public Transform lightBulb;
 	public float angularSpeed = 2.0f;
 	public float xOffset = 270.0f;
 	public float yOffset = 180.0f;
@@ -30,22 +31,66 @@ public class PetController : MonoBehaviour {
 		TurnOnLight,
 		TurnOffLight
 	}
+    public Animator animatorBody;
+    public Animator animatorFace;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		if (referencedWorldPos)
 			oldReferencedWorldPos = referencedWorldPos.position;
 		oldMovementMomentum = Vector3.zero;
 		state = States.Stationary;
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 		if (state == States.Action) {
-			//perform action by moving
-		} else if (state == States.Poked)
-			return;
-		else {
+            if (Vector3.Distance(transform.position, actionTarget.position)<distanceCap)
+            {
+                performAction();
+            }
+            else
+            {
+                float step = speed * Time.deltaTime;
+                Vector3 desiredMovement;
+                desiredMovement = (referencedWorldPos.position - oldReferencedWorldPos) + Vector3.MoveTowards(transform.position, actionTarget.transform.position, step);
+                Vector3 smoothenedMovement;
+                smoothenedMovement = alpha * (desiredMovement - transform.position) + beta * oldMovementMomentum;
+                transform.position += smoothenedMovement;
+                oldMovementMomentum = smoothenedMovement;
+                oldReferencedWorldPos = referencedWorldPos.position;
+            }
+            Vector3 targetDir = actionTarget.position - transform.position;
+
+            // The step size is equal to speed times frame time.
+            float angularStep = angularSpeed * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, angularStep, 0.0f);
+            Debug.DrawRay(transform.position, newDir, Color.red);
+
+            // Move our position a step closer to the target.
+            transform.rotation = Quaternion.LookRotation(newDir);
+        }
+        if (state == States.Poked)
+        {
+            float step = speed * Time.deltaTime;
+            Vector3 desiredMovement;
+            desiredMovement = (referencedWorldPos.position - oldReferencedWorldPos) + Vector3.MoveTowards(transform.position, target.transform.position, 0);
+            Vector3 smoothenedMovement;
+            smoothenedMovement = alpha * (desiredMovement - transform.position) + beta * oldMovementMomentum;
+            transform.position += smoothenedMovement;
+            oldMovementMomentum = smoothenedMovement;
+            oldReferencedWorldPos = referencedWorldPos.position;
+            Vector3 targetDir = forwardTarget.position - transform.position;
+
+            // The step size is equal to speed times frame time.
+            float angularStep = angularSpeed * 3 * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, angularStep, 0.0f);
+            Debug.DrawRay(transform.position, newDir, Color.red);
+
+            // Move our position a step closer to the target.
+            transform.rotation = Quaternion.LookRotation(newDir);
+        }
+        else{
 			float step = speed * Time.deltaTime;
 			Vector3 desiredMovement;
 			desiredMovement = (referencedWorldPos.position - oldReferencedWorldPos) + Vector3.MoveTowards (transform.position, target.transform.position, step);
@@ -94,33 +139,52 @@ public class PetController : MonoBehaviour {
 				// Move our position a step closer to the target.
 				transform.rotation = Quaternion.LookRotation (newDir);
 			}
-		}
+        }
 	}
 
 	void changeState(States newState){
 		state = newState;
-		//Debug.Log ("State is now "+newState.ToString());
+		Debug.Log ("State is now "+newState.ToString());
 		if (newState==States.Action){
-			//Set animator trigger
+            //Set animator trigger
+            animatorBody.SetTrigger("action");
+            animatorFace.SetTrigger("action");
 		}
 		else if (newState==States.Moving){
-			//Set animator trigger
-		}
+            //Set animator trigger
+            animatorBody.SetTrigger("move");
+            animatorFace.SetTrigger("normal");
+        }
 		else if (newState==States.Stationary){
-			//Set animator trigger
-		}
+            //Set animator trigger
+            animatorBody.SetTrigger("stay");
+            animatorFace.SetTrigger("normal");
+        }
 		else if (newState==States.Poked){
-			//Set animator trigger
-		}
+            //Set animator trigger
+            animatorBody.SetTrigger("stay");
+            animatorFace.SetTrigger("poked");
+            //timer?
+        }
 	}
 
-	public void performAction(Action newAction){
-		action = newAction;
-		changeState (States.Action);
+	public void prepareAction(Action newAction){
+        if (newAction == Action.TurnOnLight && lightBulb)
+        {
+            actionTarget = lightBulb;
+            changeState(States.Action);
+        }
+		
 	}
 
-	public void endPoked(){
-		if (action != null) {
+    public void performAction()
+    {
+        actionTarget = null;
+        // animation trigger
+    }
+
+    public void endPoked(){
+		if (actionTarget != null) {
 			changeState (States.Action);
 		} else if (Vector3.Distance (target.transform.position, transform.position) <= distanceCap) {
 			changeState (States.Moving);
@@ -136,4 +200,9 @@ public class PetController : MonoBehaviour {
 			changeState (States.Stationary);
 		}
 	}
+
+    public void OnMouseDown()
+    {
+        changeState(States.Poked);
+    }
 }
