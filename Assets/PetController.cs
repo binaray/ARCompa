93d5168d13df351e99672df001b1dee3cb6882ef
yuetung <limyuettung@hotmail.com>
2018-09-29 17:08:35 +0800

    smooth

--- Assets/PetController.cs
+++ Assets/PetController.cs
@@ -11,23 +11,34 @@ public class PetController : MonoBehaviour {
 	public float minThreshold = 0.05f;
 	public float maxThreshold = 0.5f;
 	private Vector3 oldReferencedWorldPos;
+	private Vector3 oldMovementMomentum;
+	public float alpha = 0.6f;
+	public float beta = 0.4f;
 
 	// Use this for initialization
 	void Start () {
 		if (referencedWorldPos)
 			oldReferencedWorldPos = referencedWorldPos.position;
+		oldMovementMomentum = Vector3.zero;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (referencedWorldPos && Vector3.Distance(referencedWorldPos.position, oldReferencedWorldPos)>=minThreshold &&
-			Vector3.Distance(referencedWorldPos.position, oldReferencedWorldPos)<=maxThreshold)
-			transform.position += referencedWorldPos.position - oldReferencedWorldPos;
-		else if (Vector3.Distance (target.transform.position, transform.position) > distanceCap) {
+		float step = speed * Time.deltaTime;
+		Vector3 desiredMovement = (referencedWorldPos.position - oldReferencedWorldPos) + Vector3.MoveTowards (transform.position, target.transform.position, step);
+		Vector3 smoothenedMovement;
+		if (referencedWorldPos && Vector3.Distance (referencedWorldPos.position, oldReferencedWorldPos) >= minThreshold &&
+		    Vector3.Distance (referencedWorldPos.position, oldReferencedWorldPos) <= maxThreshold) {
+			smoothenedMovement = Vector3.zero;
+		}
+		smoothenedMovement = alpha*(desiredMovement-transform.position)+beta*oldMovementMomentum;
+		transform.position += smoothenedMovement;
+		/*if (Vector3.Distance (target.transform.position, transform.position) > distanceCap) {
 			float step = speed * Time.deltaTime;
 			// Move our position a step closer to the target.
 			transform.position = Vector3.MoveTowards (transform.position, target.transform.position, step);
-		}
+		}*/
+		oldMovementMomentum = smoothenedMovement;
 		oldReferencedWorldPos = referencedWorldPos.position;
 	}
 }
